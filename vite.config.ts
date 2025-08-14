759c93822d76f4ae22730eef5a8796778b560080
renton352 <flubber777@icloud.com>
2025-08-14 22:28:34 +0900

    UP

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// GitHub Pages 用にベースを固定（あなたのリポジトリ名）
 export default defineConfig({
   base: '/nfc-photo-frame/',
   plugins: [react()],
